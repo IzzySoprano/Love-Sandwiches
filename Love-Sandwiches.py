13fd361514cb06d9da85697afbdc9e2423c5f133
Sador1993 <75792132+Sador1993@users.noreply.github.com>
2021-09-22 17:23:39 +0100

    Test

--- Love-Sandwiches.py
+++ Love-Sandwiches.py
@@ -7,14 +7,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-    CREDS = Credentials.from_service_account_file('creds.json')
-    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-    GPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-    SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-    sales = SHEET.worksheet('sales')
+sales = SHEET.worksheet('sales')
 
-    data = sales.get_all_values()
+data = sales.get_all_values()
 
-    print(data)
+print(data)
 
+print("100")
