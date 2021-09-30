fa7ba150cf5daee2eb746a61018876b08975e67b
Sador1993 <75792132+Sador1993@users.noreply.github.com>
2021-09-30 18:46:10 +0100

    Refactor update worksheet functions into one

--- Love-Sandwiches.py
+++ Love-Sandwiches.py
@@ -2,6 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
+from gspread.models import Worksheet
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -74,7 +76,16 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully\n")
-    
+
+def update_worksheet(data,worksheet):
+    """
+    Recieves al ist of integers to be inserted into the worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -101,10 +112,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
-
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
